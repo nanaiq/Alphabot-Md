7c7d003f88753941391c6b19686295426f21ff2a
nanaiq <108739585+nanaiq@users.noreply.github.com>
2022-07-09 14:35:53 +0700

    Update settings.js

--- settings.js
+++ settings.js
@@ -13,8 +13,8 @@ global.APIKeys = {
 }
 
 // setting 
-global.autoread = false // auto read pesan / message
-global.autobio = true 
+global.autoread = true // auto read pesan / message
+global.autobio = false 
 
 //language
 //Available in indonesia & english
@@ -23,23 +23,23 @@ global.language = english  //change indonesia to english if you don't understand
 
 
 // Other
-global.botname = "Alphabot-Mdོ" //namabot kalian
-global.ownername= "ᴹᴿ᭄ ZeeoneOfcོ ×፝֟͜×" //nama kalian
-global.myweb ="https://api.zeeoneofc.xyz" //bebas asal jan hapus
-global.youtube = "https://youtube.com/c/ZeeoneOfc" //bebas asal jan hapus
-global.github = "https://zeeone-ofc.github.io/" //bebas
-global.email = "zeeoneofc@gmail.com" //bebas
-global.region = "Indonesia" //bebas
+global.botname = "X-ZC" //namabot kalian
+global.ownername= "X" //nama kalian
+global.myweb ="http://tiny.cc/X-ZC" //bebas asal jan hapus
+global.youtube = "http://tiny.cc/Nathalie" //bebas asal jan hapus
+global.github = "https://xxx.com" //bebas
+global.email = "Jancog@gmail.com" //bebas
+global.region = "Pluto" //bebas
 global.timezone = 'Asia/Jakarta' //  timezone wib
-global.owner = ["622150996855","622150996855"] //ganti agar fitur owner bisa di gunakan
-global.ownernomer = "62887435047326" // nomor wa kalian
-global.ownernomerr = "+62887435047326" //nmr wa kalian
+global.owner = ["6285641476033","6285641476033"] //ganti agar fitur owner bisa di gunakan
+global.ownernomer = "6285641476033" // nomor wa kalian
+global.ownernomerr = "+6285641476033" //nmr wa kalian
 global.thumbnail = "./image/lol.jpg" // ini lol.jpg adalah nama foto di folder image. untuk foto bot
 global.donasi = "./image/donasi.jpg" // foto donasi di folder image
-global.background_welcome="https://telegra.ph/file/90a931648de597820bc08.jpg" // maks size 30kb, agar welcome image nya tdk delay
+global.background_welcome="https://telegra.ph/file/6daffe7a858629323dd10.jpg" // maks size 30kb, agar welcome image nya tdk delay
 
-global.packname = '© Alphabot-Mdོ' //sticker wm ubah
-global.author = 'Di Buat Oleh ZeeoneOfc' //sticker wm ganti nama kalian
+global.packname = 'X-ZC' //sticker wm ubah
+global.author = 'IG @hai_om.qw' //sticker wm ganti nama kalian
 global.sessionName = 'session'
 global.typemenu = 'document'
 global.flaming = 'https://www6.flamingtext.com/net-fu/proxy_form.cgi?&imageoutput=true&script=sketch-name&doScale=true&scaleWidth=800&scaleHeight=500&fontsize=100&text='
